dc2df1e99d04773bef96367163d63c431d6ccbb6
Jake Baker <jxbaker@sep.com>
2022-12-15 08:27:19 -0500

    Day 15 part 2

--- Days/Day15/Day15.cs
+++ Days/Day15/Day15.cs
@@ -18,36 +18,72 @@ public class Day15 : AdventOfCode<long, List<(Position Sensor, Position Beacon)>
     }
 
 
-    [TestCase(Input.Example, 26)]
-    [TestCase(Input.File, 5832528)]
+    // [TestCase(Input.Example, 26)]
+    // [TestCase(Input.File, 5832528)]
     public override long Part1(List<(Position Sensor, Position Beacon)> input)
     {
         var row = input[0].Sensor.X == 2 ? 10 : 2000000;
         var beacons = input.Select(it => it.Beacon).ToHashSet();
-        var sensorToManhattanDistance = input.Select(it => (it.Sensor, it.Sensor.ManhattanDistance(it.Beacon)))
-            .ToDictionary(it => it.Item1, it => it.Item2);
+        var s2md = input.Select(it => new {Sensor = it.Sensor, Md = it.Sensor.ManhattanDistance(it.Beacon)})
+            .ToList();
         
-        var minx = sensorToManhattanDistance.Select(it => it.Key.X - it.Value).Min();
-        var maxx = sensorToManhattanDistance.Select(it => it.Key.X + it.Value).Max();
+        var minx = s2md.Select(it => it.Sensor.X - it.Md).Min();
+        var maxx = s2md.Select(it => it.Sensor.X + it.Md).Max();
 
-        var count = 0;
-        for (var x = minx; x <= maxx; x++)
+        var scanned = new HashSet<Position>();
+        foreach (var item in s2md)
         {
-            if (sensorToManhattanDistance.Any(it => it.Key.ManhattanDistance(new(row, x)) <= it.Value))
+            var xsq = XsWithinManhattanDistance(item.Sensor, row, item.Md);
+            if (xsq is not {} xs) continue;
+            for (var x = xs.Item1; x <= xs.Item2; x++)
             {
-                count += 1;
+                scanned.Add(new(row, x));
             }
         }
 
-        return count - beacons.Count(beacon => beacon.Y == row);
+        return scanned.Count - beacons.Count(beacon => beacon.Y == row);
     }
 
 
-    [TestCase(Input.Example, 0)]
-    [TestCase(Input.File, 0)]
+    // [TestCase(Input.Example, 56000011)]
+    // [TestCase(Input.File, 13360899249595)]
     public override long Part2(List<(Position Sensor, Position Beacon)> input)
     {
-        return 0;
+        var maxCoordinate = input[0].Sensor.X == 2 ? 20 : 4_000_000;
+        var tuningMultiplier = 4000000;
+        var s2md = input.Select(it => new {Sensor = it.Sensor, Md = it.Sensor.ManhattanDistance(it.Beacon)})
+            .ToList();
+        
+        for(var y = 0; y <= maxCoordinate; y++)
+        {
+            if (y % 100000 == 0) Console.WriteLine(y);
+            var xss = s2md.Select(it => XsWithinManhattanDistance(it.Sensor, y, it.Md))
+                .OfType<(long, long)>()
+                .ToList();
+            foreach(var xs in xss)
+            {
+                var x = xs.Item1 - 1;
+                if (x >= 0 && x <= maxCoordinate && !xss.Any(xs2 => x >= xs2.Item1 && x <= xs2.Item2))
+                {
+                    return x * tuningMultiplier + y;
+                }
+                x = xs.Item2 + 1;
+                if (x >= 0 && x <= maxCoordinate && !xss.Any(xs2 => x >= xs2.Item1 && x <= xs2.Item2))
+                {
+                    return x * tuningMultiplier + y;
+                }
+            }
+        }
+
+        throw new ApplicationException();
+    }
+
+    private (long, long)? XsWithinManhattanDistance(Position p, long row, long distance)
+    {
+        var delta = Math.Abs(p.Y - row);
+        if (delta > distance) return null;
+        var width = distance - delta;
+        return (p.X - width, p.X + width);
     }
 }
 
